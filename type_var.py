565538f21dc8b7057e2041925320e3cb1176d7e6
Maxim <potolki33@bk.ru>
2021-07-29 21:18:22 +0300

    Type commit

---
+++ type_var.py
@@ -0,0 +1,17 @@
+# Типы переменных
+
+# Марка
+name = 'Ford'
+print(name, type(name))
+
+# Возраст
+age = 3
+print(age, type(age))
+
+# Объем двигателя
+engine_volume = 1.6
+print(engine_volume, type(engine_volume))
+
+# Есть ли люк
+sky = False
+print(sky, type(sky))
