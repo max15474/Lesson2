cb9a971ff91b984a92182848a9206576ea2c3875
Maxim <potolki33@bk.ru>
2021-07-29 21:17:35 +0300

    Math commit

---
+++ math.py
@@ -0,0 +1,26 @@
+a = input('First num: ')
+b = input('Second num: ')
+a = int(a)
+b = int(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+result = a / b
+print(type(result))
+print(result)
+
+c = True
+d = False
+
+print(not c)
+print(c and d)
+print(c or d)
+
+e = 10
+print(e > 100)
+print(e < 100)
+print(e >= 100)
+print(e <= 100)
+print(e == 100)
+print(e != 100)
